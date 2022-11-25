cbb35b9ff017531520a0e9b97284addf32ed0951
Neli3307 <yeduk33@gmail.com>
2022-11-25 17:04:25 +0900

    Add Item Database(JSON) & Callback Method on Clicking Item

--- YongKyu/Scripts/Inventory.gd
+++ YongKyu/Scripts/Inventory.gd
@@ -1,16 +1,42 @@
 extends Control
 
+#############
+# variables #
+#############
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var item_list = $Panel/ItemList
+onready var description_label = $Panel/DescriptionLabel
+onready var item_JSON_path = "res://YongKyu/Scripts/ItemData.JSON"
+var item_data
 
+###########
+# methods #
+###########
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	loadItemJSON()
+
+func _on_ItemList_item_selected(index):
+	description_label.set_text("")
+	var item_name = item_list.get_item_text(index)
+	var item_description = item_data[item_name]["Description"]
+	description_label.set_text(item_name + str("\n") + item_description)
+	#print(item_description)
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func loadItemJSON():
+	var file = File.new()
+	if not file.file_exists(item_JSON_path):
+		print("Error on reading ItemData.JSON file. Please check.")
+		get_tree().paused = true
+		return
+	file.open(item_JSON_path, File.READ)
+	var JSON_parsed = JSON.parse(file.get_as_text())
+	if typeof(JSON_parsed.result) != TYPE_DICTIONARY:
+		print("Unexpected result on reading ItemData.JSON file. Please check.")
+		print("Type of result is " + str(typeof(JSON_parsed.result)))
+		get_tree().paused = true
+		return
+	file.close()
+	item_data = JSON_parsed.result
