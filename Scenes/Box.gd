dd8db0b5970e36bae10d55595d81efca434ade2a
Neli3307 <yeduk33@gmail.com>
2022-12-15 04:08:00 +0900

    Restoring...

--- Scenes/Box.gd
+++ Scenes/Box.gd
@@ -1,8 +1,9 @@
 extends StaticBody2D
 
 export (Array) var get_item_list
+export (int) var placed_index
 
-signal gotIt
+signal gotIt(list)
 signal get_item_from_map(list)
 
 var already = false
@@ -12,12 +13,20 @@ func _ready():
 	self.connect("gotIt", player.get_node("Camera2D").get_node("Gotcha"), "_on_gotIt")
 	self.connect("get_item_from_map", player.get_node("PlayerUI").get_node("IntegratedInventory"), "_on_get_item_from_map")
 
+	if not PlacedItem.is_placed(placed_index):
+		$light.visible = false
+		$Unopen.visible = false
+		$Open.visible = true
+		$Button.visible = false
+		
+		already = true
+
 func _on_Area2D_body_entered(body):
 	if body is Player && already == false:
 		$Button.visible = true
 
 func _on_Button_pressed():
-	emit_signal("gotIt")
+	emit_signal("gotIt", get_item_list)
 	emit_signal("get_item_from_map", get_item_list)
 	$light.visible = false
 	$Unopen.visible = false
@@ -25,3 +34,5 @@ func _on_Button_pressed():
 	$Button.visible = false
 	
 	already = true
+	
+	PlacedItem.unplace_by_index(placed_index)
