df18fb3ab94859eb1e6a792da9c01ad0a7c469af
Neli3307 <yeduk33@gmail.com>
2022-12-14 22:01:40 +0900

    Add Placed Item List Manage - Done

--- Eunseo/Scenes/Box.gd
+++ Eunseo/Scenes/Box.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 export (Array) var get_item_list
+export (int) var placed_index
 
 signal gotIt
 signal get_item_from_map(list)
@@ -11,6 +12,14 @@ func _ready():
 	var player = get_tree().current_scene.get_node("player")
 	self.connect("gotIt", player.get_node("Camera2D").get_node("Gotcha"), "_on_gotIt")
 	self.connect("get_item_from_map", player.get_node("PlayerUI").get_node("IntegratedInventory"), "_on_get_item_from_map")
+
+	if not PlacedItem.is_placed(placed_index):
+		$light.visible = false
+		$Unopen.visible = false
+		$Open.visible = true
+		$Button.visible = false
+		
+		already = true
 
 func _on_Area2D_body_entered(body):
 	if body is Player && already == false:
@@ -25,3 +34,5 @@ func _on_Button_pressed():
 	$Button.visible = false
 	
 	already = true
+	
+	PlacedItem.unplace_by_index(placed_index)
