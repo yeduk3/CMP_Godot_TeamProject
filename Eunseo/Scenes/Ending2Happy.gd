a56847c283d8acd3a4e5daeed790db0e6b519563
Yang-es <insultas@ajou.ac.kr>
2022-12-15 20:39:50 +0900

    happyEnding final

--- Eunseo/Scenes/Ending2Happy.gd
+++ Eunseo/Scenes/Ending2Happy.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var sprite_tween
 var bright_tween
+var ending_tween
 
 var afterStart = false
 
@@ -25,7 +26,30 @@ func _ready():
 	# bright off
 	bright_tween.tween_property(bright_node, "color", Color(0, 0, 0, 0), 2).set_delay(3)
 	
+	yield(get_tree().create_timer(7),"timeout")
 	afterStart = true
 	
 	if afterStart == true:
+		print("dialog")
 		$Dialog.use_Happy()
+		
+
+func _on_DialogBox_hide_Dialog():
+	sprite_tween = create_tween()
+	var sprite_node1 = get_node("After/AfterPlayer")
+	var sprite_node2 = get_node("After/AfterGirl")
+	sprite_tween.tween_property(sprite_node1, "global_position:y", sprite_node1.global_position.y - 250, 1)
+	sprite_tween.parallel()
+	sprite_tween.tween_property(sprite_node2, "global_position:y", sprite_node2.global_position.y - 250, 1)
+	
+	yield(get_tree().create_timer(1),"timeout")
+	
+	ending_tween = create_tween()
+	var ending_node = get_node("EndingBackground")
+	ending_tween.tween_property(ending_node, "color", Color(1, 1, 1, 1), 2)
+	
+	yield(get_tree().create_timer(3),"timeout")
+	$EndingBackground/EndingText.visible = true
+
+	yield(get_tree().create_timer(5),"timeout")
+	get_tree().change_scene("res://Eunseo/Scenes/Menu.tscn")
