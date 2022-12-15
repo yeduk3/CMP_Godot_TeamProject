0702747dc121e91a2b068547d8900a25b48d5518
Yang-es <insultas@ajou.ac.kr>
2022-12-16 01:26:19 +0900

    add Attack

--- monster_final.gd
+++ monster_final.gd
@@ -8,6 +8,7 @@ var speed = 170
 var motion = Vector2.ZERO
 var play = null
 var attackk = false
+var damage = false
 onready var sprite = $AnimatedSprite
 onready var player = get_node("../../Player")
 
@@ -49,6 +50,15 @@ func _process(delta):
 		elif player.position < position:
 			sprite.flip_h = true
 			sprite.play("walk")
+	
+	if sprite.animation == "attack" and sprite.frame == 3 and not damage:
+		print("Attackakakakak")
+		damage = true
+		# if player is in area
+		if play:
+			var health = $"../../Player/PlayerUI/HealthBarTest/Health"
+			health._health_down(10)
+	
 
 
 
@@ -66,24 +76,25 @@ func _on_Timer_timeout():
 func _on_Area2D_body_entered(body):
 	if "Map" in body.name:
 		return
-	print("entered")
-	attackk = true
-	# 3초 무적이면 3000, 5초 무적이면 5000......
-	if time_hit + 5000 > Time.get_ticks_msec():
-		print('super')
-	else:
-		var health = $"../../Player/PlayerUI/HealthBarTest/Health"
-		health._health_down(10)
-		time_hit = Time.get_ticks_msec()
-		print('hit')
-		_unbeatable_timer()
-
-
+	if body is Player:
+		print("entered")
+		play = body
+		attackk = true
+		# 3초 무적이면 3000, 5초 무적이면 5000......
+		if time_hit + 5000 > Time.get_ticks_msec():
+			print('super')
+		else:
+			#var health = $"../../Player/PlayerUI/HealthBarTest/Health"
+			#health._health_down(10)
+			time_hit = Time.get_ticks_msec()
+			print('hit')
+			_unbeatable_timer()
 
 func _on_Area2D_body_exited(body):
-	print("exited")
-	play = null
-	attackk = false
+	if body is Player:
+		print("exited")
+		play = null
+		attackk = false
 
 func _unbeatable_timer():
 	var health = $"../../Player/PlayerUI/HealthBarTest/Health"
@@ -99,3 +110,7 @@ func _unbeatable_timer():
 	set_collision_mask_bit(0, true)
 	$"../../Player".set_collision_mask_bit(2, true)
 	#print("Unbeatable Timer off")
+
+
+func _on_AnimatedSprite_animation_finished():
+	damage = false
